02d3706a3eadd228232303097a6576ac679d7794
Priyanshu Sharma <priyanshushrama2006@gmail.com>
2026-07-21 23:37:44 +0530

    Update dmq2_reading.ino

--- software/arduino_ide_code/esp32-code-testing1/mq2_reading.ino
+++ software/arduino_ide_code/esp32-code-testing1/mq2_reading.ino
@@ -1,14 +1,54 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+
+// WiFi credentials
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+// Flask server
+const char* serverName = "http://YOUR_LOCAL_IP:5000/predict";
+
+// MQ2 sensor pin
 const int mq2Pin = 34;
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("MQ-2 Sensor Starting...");
-}
-void loop() {
-  int gasValue = analogRead(mq2Pin);
-  Serial.print("Gas Value: ");
-  Serial.println(gasValue);
 
-  delay(1000);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  int mq2Value = analogRead(mq2Pin);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonData = "{\"mq2\":" + String(mq2Value) + "}";
+
+    int responseCode = http.POST(jsonData);
+
+    if (responseCode > 0) {
+      String response = http.getString();
+      Serial.println(response);
+    } else {
+      Serial.print("HTTP Error: ");
+      Serial.println(responseCode);
+    }
+
+    http.end();
+  }
+
+  delay(4000);
 }
